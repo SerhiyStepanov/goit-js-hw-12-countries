e0e45fd91c3d3c063c42c7935f692484bba71569
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-30 20:56:46 +0200

    add debounce

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const debounce = require('lodash.debounce');
+
+const inputEl = document.querySelector('.input-control')
+// console.log(inputEl)
+
+inputEl.addEventListener('input', debounce(onInputCountry,500))
+
+function onInputCountry(event) {
+    const name = event.target.value
+    console.log(event.target.value)
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then(res => { return res.json() })
+        .then(country => {
+            console.log(country)
+})
+}
