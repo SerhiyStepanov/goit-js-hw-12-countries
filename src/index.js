5cd406944c74d1f0c33bdea707bbcdf762e2720e
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-31 14:41:02 +0200

    bed work

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-// import countryCard from './template.hbs'
+import countryCard from './template.hbs'
 const debounce = require('lodash.debounce');
-
 
 const inputEl = document.querySelector('.input-control')
 // console.log(inputEl)
-
 inputEl.addEventListener('input', debounce(onInputCountry,500))
+
 
 function onInputCountry(event) {
     const name = event.target.value
-    console.log(event.target.value) 
+    // console.log(event.target.value) 
 
     fetch(`https://restcountries.eu/rest/v2/name/${name}`)
     .then(res => { return res.json() })
     .then(country => {
         console.log(country)
-        // const markupCart = countryCard(country)
+        const markup = countryCard(country)
+        console.log(markup)
     })
 }
-
-
 
 
