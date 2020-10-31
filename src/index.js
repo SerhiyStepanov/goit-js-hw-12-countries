a5607fde9f50b6a994d8c54842dc23cf278e3d3d
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-31 20:58:33 +0200

    error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import countryCard from './template.hbs'
-// import styles from './styles.css'
 import fetchCountry from './fetchCountries'
+
 const debounce = require('lodash.debounce');
 
 const inputEl = document.querySelector('.input-control')
 const markapCard = document.querySelector('.container-js')
-// console.log(inputEl)
+
 inputEl.addEventListener('input', debounce(onInputCountry,500))
 
 
 function onInputCountry(event) {
     const name = event.target.value
-    // console.log(countryName)
+
     fetchCountry(name)
         .then(renderCountryCard)
         .catch()
