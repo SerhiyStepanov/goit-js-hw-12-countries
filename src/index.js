9a826ee090f621ce3a7f0ba4f3fe3e84cd012585
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-01 13:46:33 +0200

    add pnotify

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import countryCard from './template.hbs'
 import fetchCountry from './fetchCountries'
+import notify from '@pnotify/core'
+const debounce = require('lodash.debounce')
+import './styles.css'
 
-const debounce = require('lodash.debounce');
 
 const inputEl = document.querySelector('.input-control')
 const markapCard = document.querySelector('.container-js')
 
 inputEl.addEventListener('input', debounce(onInputCountry,500))
 
+notify.styling = 'material'
+notify.icons = 'material'
 
 function onInputCountry(event) {
     const name = event.target.value
 
     fetchCountry(name)
-        .then(renderCountryCard)
+        .then(renderCountryCard
+        )
         .catch()
         .finally()
 }
