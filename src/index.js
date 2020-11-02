95b50fbc7c41ab43f8fff2b1330038e3ab271540
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-02 14:26:09 +0200

    del default

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import '@pnotify/core/dist/Material.css';
 import { defaults } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css'
 import '@pnotify/mobile/dist/PNotifyMobile.css'
-import { alert, defaultModules } from '@pnotify/core/dist/PNotify.js'
+import { alert } from '@pnotify/core/dist/PNotify.js'
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js'
 
 import './styles.css'
